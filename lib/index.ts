74c4c36a14b78c8b61d36ad9edc41b23cf964ed1
shprink <julien.renaux@gmail.com>
2015-11-16 19:51:32 -0600

    reoganize components to be feature based instead of being type based

--- lib/index.ts
+++ lib/index.ts
@@ -8,7 +8,7 @@ import 'es6-shim';
 import {bootstrap, provide, FORM_PROVIDERS} from 'angular2/angular2';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
-import {App} from './components/app/index';
+import {App} from './app/index';
 
 bootstrap(App, [
     FORM_PROVIDERS,
